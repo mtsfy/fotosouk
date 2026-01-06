514763b66f9e5144963acc8f12a004b78cd82c4e
Muse Tesfaye <tesfaye.musee@gmail.com>
2026-01-06 16:16:52 -0500

    refactor: image transformation

--- go.mod
+++ go.mod
@@ -3,12 +3,23 @@ module github.com/mtsfy/fotosouk
 go 1.24.4
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.41.0
+	github.com/aws/aws-sdk-go-v2/config v1.32.6
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.95.0
+	github.com/disintegration/imaging v1.6.2
+	github.com/gofiber/contrib/jwt v1.1.2
+	github.com/gofiber/fiber/v2 v2.52.10
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.31.0
+	gorm.io/driver/postgres v1.6.0
+	gorm.io/gorm v1.31.1
+)
+
+require (
 	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/aws/aws-sdk-go v1.55.8 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.41.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.4 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.32.6 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.19.6 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.16 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.16 // indirect
@@ -19,15 +30,11 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.16 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.16 // indirect
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.95.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/signin v1.0.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.30.8 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.12 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.41.5 // indirect
 	github.com/aws/smithy-go v1.24.0 // indirect
-	github.com/gofiber/contrib/jwt v1.1.2 // indirect
-	github.com/gofiber/fiber/v2 v2.52.10 // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -35,8 +42,6 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -45,11 +50,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/image v0.34.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
-	gorm.io/driver/postgres v1.6.0 // indirect
-	gorm.io/gorm v1.31.1 // indirect
 )
--- internal/transformer/transformer.go
+++ internal/transformer/transformer.go
@@ -1,55 +1,93 @@
 package transformer
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"image"
-	"image/draw"
-	_ "image/jpeg"
-	_ "image/png"
+	"image/color"
+	"image/jpeg"
+	"image/png"
+	"strings"
 
-	xdraw "golang.org/x/image/draw"
+	"github.com/disintegration/imaging"
 )
 
 type Transformer interface {
-	Crop(ctx context.Context, img image.Image, x, y, width, height int) (image.Image, error)
-	Resize(ctx context.Context, img image.Image, width, height int) (image.Image, error)
+	Crop(ctx context.Context, imgData []byte, width, height int, format string) ([]byte, error)
+	Resize(ctx context.Context, imgData []byte, width, height int, format string) ([]byte, error)
+	Rotate(ctx context.Context, imgData []byte, degrees int, format string) ([]byte, error)
 }
 
 type ImageTransformer struct{}
 
-func (t *ImageTransformer) Crop(ctx context.Context, img image.Image, x, y, width, height int) (image.Image, error) {
-	if width <= 0 || height <= 0 {
-		return nil, errors.New("width and height must be positive")
+func (t *ImageTransformer) Crop(ctx context.Context, imgData []byte, width, height int, format string) ([]byte, error) {
+	img, err := imaging.Decode(bytes.NewReader(imgData))
+	if err != nil {
+		return nil, err
 	}
-	if x < 0 || y < 0 {
-		return nil, errors.New("x and y must be non-negative")
-	}
-
-	srcBounds := img.Bounds()
-	cropRect := image.Rect(srcBounds.Min.X+x, srcBounds.Min.Y+y, srcBounds.Min.X+x+width, srcBounds.Min.Y+y+height)
-
-	if !cropRect.In(srcBounds) {
-		return nil, errors.New("crop rectangle out of bounds")
-	}
-
-	if si, ok := img.(interface {
-		SubImage(r image.Rectangle) image.Image
-	}); ok {
-		return si.SubImage(cropRect), nil
-	}
-
-	dst := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(dst, dst.Bounds(), img, cropRect.Min, draw.Src)
-	return dst, nil
+	cropped := imaging.CropCenter(img, width, height)
+	return encodeImage(cropped, format)
 }
 
-func (t *ImageTransformer) Resize(ctx context.Context, img image.Image, width, height int) (image.Image, error) {
-	if width <= 0 || height <= 0 {
-		return nil, errors.New("width and height must be positive")
+func (t *ImageTransformer) Resize(ctx context.Context, imgData []byte, width, height int, format string) ([]byte, error) {
+	img, err := imaging.Decode(bytes.NewReader(imgData))
+	if err != nil {
+		return nil, err
+	}
+	resized := imaging.Resize(img, width, height, imaging.Lanczos)
+	return encodeImage(resized, format)
+}
+
+func (t *ImageTransformer) Rotate(ctx context.Context, imgData []byte, degrees int, format string) ([]byte, error) {
+	img, err := imaging.Decode(bytes.NewReader(imgData))
+	if err != nil {
+		return nil, err
 	}
 
-	dst := image.NewRGBA(image.Rect(0, 0, width, height))
-	xdraw.CatmullRom.Scale(dst, dst.Bounds(), img, img.Bounds(), xdraw.Over, nil)
-	return dst, nil
+	degrees = degrees % 360
+	if degrees < 0 {
+		degrees += 360
+	}
+
+	var rotated image.Image
+	switch degrees {
+	case 90:
+		rotated = imaging.Rotate90(img)
+	case 180:
+		rotated = imaging.Rotate180(img)
+	case 270:
+		rotated = imaging.Rotate270(img)
+	case 0:
+		rotated = img
+	default:
+		rotated = imaging.Rotate(img, float64(degrees), color.Transparent)
+	}
+
+	return encodeImage(rotated, format)
+}
+
+func encodeImage(img image.Image, format string) ([]byte, error) {
+	var buf bytes.Buffer
+	format = strings.ToLower(format)
+
+	switch format {
+	case "png", "image/png":
+		err := png.Encode(&buf, img)
+		return buf.Bytes(), err
+	case "jpeg", "jpg", "image/jpeg":
+		err := jpeg.Encode(&buf, img, &jpeg.Options{Quality: 95})
+		return buf.Bytes(), err
+	default:
+		return nil, errors.New("unsupported format: use png or jpeg")
+	}
+}
+
+func GetImageSize(imgData []byte) (width, height int, err error) {
+	img, err := imaging.Decode(bytes.NewReader(imgData))
+	if err != nil {
+		return 0, 0, err
+	}
+	bounds := img.Bounds()
+	return bounds.Dx(), bounds.Dy(), nil
 }
